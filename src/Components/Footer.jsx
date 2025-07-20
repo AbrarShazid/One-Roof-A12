cc9cbd1799fc16511e93a8e87bbf22cbb6167a8d
Abrar Agmain Shazid <abraragmain75@gmail.com>
2025-07-20 22:26:34 +0600

    style fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -36,7 +36,7 @@ const Footer = () => {
             <h3 className="text-lg font-bold">Quick Links</h3>
             <ul className="space-y-2">
               <li><Link to="/" className='hover:border hover:border-b-white hover:opacity-80 transition-opacity text-sm' >Home</Link></li>
-              <li><Link to="/apartments" className=' hover:border-b-white hover:opacity-80 transition-opacity text-sm'>Apartments</Link></li>
+              <li><Link to="/apartments" className='hover:border-b hover:border-white hover:opacity-80 transition-opacity text-sm'>Apartments</Link></li>
               <li>  <Link to="/#about" className="hover:border hover:border-b-white hover:opacity-80 transition-opacity text-sm">
                 About Us
               </Link></li>
